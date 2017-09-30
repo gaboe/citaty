b734bb4c4f836f6264a34e7de16b39e2c2d1c941
Gabriel Ecegi <gabriel.ecegi@blogic.cz>
2017-09-30 23:01:16 +0200

    add channel query

---
+++ src/Quotes.Data.GraphQL/Models/ChannelType.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Types;
+using Quotes.Data.Domain.Models;
+
+namespace Quotes.Data.GraphQL.Models
+{
+    public class ChannelType : ObjectGraphType<Channel>
+    {
+        public ChannelType()
+        {
+            Field(x => x.ChannelID).Description("ID of channel");
+            Field(x => x.Quotes, true, typeof(ListGraphType<QuoteType>)).Description("Enumerable of quotes");
+            Field(x => x.Title).Description("Title of quotes");
+        }
+    }
+}
